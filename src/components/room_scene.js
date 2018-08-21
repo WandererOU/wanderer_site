864b568fc533d0ff55afd8b42e8c2c9ef564f7fe
Andre Vasconcelos <andre.tito@architectgroup.com>
2018-08-21 15:12:39 +0900

    Added initial mapping for locations, improved layouts and styling

--- src/components/room_scene.js
+++ src/components/room_scene.js
@@ -3,7 +3,9 @@ var OBJLoader = require('three-obj-loader');
 var MTLLoader = require('three-mtl-loader');
 OBJLoader(THREE);
 
+import {TweenLite} from 'gsap';
 import {loadProgressPage, removeProgressPage, updateProgress} from './loading_screen';
+import * as constants from '../utils/constants';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 2000 );
@@ -12,21 +14,72 @@ var renderer = new THREE.WebGLRenderer();
 var manager = new THREE.LoadingManager();
 var objectLoader = new THREE.OBJLoader(manager);
 var mtlLoader = new MTLLoader(manager);
-// var particleSystem;
 
-// Event listeners to handle changes & interaction
-window.addEventListener('resize', () => { 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-});
-
-document.addEventListener('mousemove', (event) => {
-    var x = event.movementX;
-    var y = event.movementY;
-    var sensitivity = 0.00002;
-    camera.rotation.y -= x * sensitivity;
-    camera.rotation.x -= y * sensitivity;
-    renderer.render(scene, camera)
-})
+// State used to map cursor events; coordinates are fixed locations to move camera to
+// DO NOT change value of coordinates!
+var activeMenu = constants.INITIAL;
+const menuCoordinates = {
+    initial: {
+        position :{
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        rotation : {
+            x: 0,
+            y: 0,
+            z: 0
+        }
+    },
+    about: {
+        position :{
+            x: 1,
+            y: 0,
+            z: -3
+        },
+        rotation : {
+            x: 0,
+            y: -1.5,
+            z: 0
+        }
+    },
+    apps: {
+        position :{
+            x: -1,
+            y: 0,
+            z: -5
+        },
+        rotation : {
+            x: 0,
+            y: 1.5,
+            z: 0
+        }
+    },
+    blog: {
+        position :{
+            x: -1,
+            y: 0,
+            z: -10
+        },
+        rotation : {
+            x: 0,
+            y: 1.5,
+            z: 0
+        }
+    },
+    contacts: {
+        position :{
+            x: 0,
+            y: 0,
+            z: -4
+        },
+        rotation : {
+            x: 0,
+            y: 3.2,
+            z: 0
+        }
+    }
+}
 
 export const createScene = function() {
     loadProgressPage()
@@ -77,8 +130,87 @@ export const renderRoom = function() {
     
 }
 
-// Render particles on screen
+// Event listeners to handle changes & interaction
+window.addEventListener('resize', () => { 
+    renderer.setSize( window.innerWidth, window.innerHeight );
+});
 
+document.addEventListener('mousemove', (event) => {
+    var x = event.movementX;
+    var y = event.movementY;
+    var sensitivity = 0.00002;
+    
+    if (activeMenu === constants.INITIAL) {
+        camera.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), -y * sensitivity)
+        camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
+        renderer.render(scene, camera)
+    } else if (activeMenu === constants.ABOUT) {
+        camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
+        camera.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), -y * sensitivity)
+        renderer.render(scene, camera)
+    } else if (activeMenu === constants.APPS || activeMenu === constants.BLOG) {
+        camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
+        camera.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), y * sensitivity)
+        renderer.render(scene, camera)
+    } else if (activeMenu === constants.CONTACTS) {
+        camera.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), y * sensitivity)
+        camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
+        renderer.render(scene, camera)
+    }
+})
+
+window.addEventListener('wheel', function(event) {
+    const isScrollUp = event.deltaY > 0;
+    switch (activeMenu) {
+        case constants.INITIAL:
+            isScrollUp ? moveCamera(constants.CONTACTS) : moveCamera(constants.ABOUT);
+            break;
+        case constants.ABOUT:
+            isScrollUp ? moveCamera(constants.INITIAL) : moveCamera(constants.APPS);
+            break;
+        case constants.APPS:
+            isScrollUp ? moveCamera(constants.ABOUT) : moveCamera(constants.BLOG);
+            break;
+        case constants.BLOG:
+            isScrollUp ? moveCamera(constants.APPS) : moveCamera(constants.CONTACTS);
+            break;
+        case constants.CONTACTS:
+            isScrollUp ? moveCamera(constants.BLOG) : moveCamera(constants.INITIAL);
+            break;
+    }
+})
+
+document.addEventListener('click', (event) => {
+    // Clicked menu/button to go to About section
+    if(event.target.id.includes('goToAbout')) {
+        moveCamera(constants.ABOUT);
+    }
+})
+
+// Animation loop
+function animate() {
+    camera.updateProjectionMatrix();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate()
+
+// Camera events
+function moveCamera(menu) {
+    console.log(menu)
+    activeMenu = menu;
+    let coordinates = menuCoordinates[menu]
+    let position = coordinates.position;
+    let rotation = coordinates.rotation
+
+    let posAnimation = TweenLite.to(camera.position, 2, {x: position.x, y: position.y, z: position.z})
+    posAnimation.play()
+    let rotAnimation = TweenLite.to(camera.rotation, 2, {x: rotation.x, y: rotation.y, z: rotation.z})
+    rotAnimation.play()
+}
+
+// Render particles on screen
 // export const renderParticles = function() {
 //     var particleCount = 80,
 //     texture = new THREE.TextureLoader().load("/images/particle.png"),
