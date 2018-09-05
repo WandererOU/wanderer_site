dd8e4370fb791d1153376d1206858850bf21ef73
Andre Vasconcelos <andre.tito@architectgroup.com>
2018-09-05 13:28:22 +0900

    Added menus/improved classes/rendering scene elements/ton'o shit

--- src/components/landing_screen.js
+++ src/components/landing_screen.js
@@ -6,7 +6,12 @@ import {el, setChildren} from 'redom';
 
 export default class LandingPage {
     constructor() {
+        // variables
         this.activeMenu = constants.INITIAL;
+        this.menuTransform = {
+            x: 0,
+            y: 0,
+        }
 
         // overlays
         this.el = el('.overlay-container');
@@ -19,7 +24,7 @@ export default class LandingPage {
         this.botContainer = el('.bot-container');
 
         // layout components
-        this.logo = el('img', {src: WhiteLogo});
+        this.logo = el('img#initial', {src: WhiteLogo});
         this.titleSection = el('.title-container', [
             el('.title', [
                 el('p', 'Augmented Reality'),
@@ -34,9 +39,17 @@ export default class LandingPage {
                 )
             ]),
         ]);
+
+        this.menuContainer = el('#menu-container.menu-container', [
+            el('p#about', '01 About'),
+            el('p#apps', '02 Apps'),
+            el('p#blog', '03 Blog'),
+            el('p#contacts', '04 Contacts')
+        ]);
+
         this.startButton = el('#start-button#about.start-button', [
             el('span#about', 'Click here or scroll down to begin')
-        ])
+        ]);
     }
 
     onmount() {
@@ -46,14 +59,32 @@ export default class LandingPage {
 
     onremount() {
         this.clearElements();
+
         if(this.activeMenu === constants.INITIAL) {
-            this.renderInitialElements()
+            this.renderInitialElements();
+
+            document.getElementById('menu-container').children[0].classList.add('initial');
+            document.getElementById('menu-container').children[1].classList.add('initial');
+            document.getElementById('menu-container').children[2].classList.add('initial');
+            document.getElementById('menu-container').children[3].classList.add('initial');
         } else {
-            this.renderAlternateElements()
+            this.renderAlternateElements();
+            
+            document.getElementById('menu-container').children[0].classList.remove('initial');
+            document.getElementById('menu-container').children[1].classList.remove('initial');
+            document.getElementById('menu-container').children[2].classList.remove('initial');
+            document.getElementById('menu-container').children[3].classList.remove('initial');
+            document.getElementById('menu-container').querySelector(`#${this.activeMenu}`).classList.add('selected');
         }
     }
 
     clearElements = () => {
+        if (document.getElementById('menu-container')) {
+            for(let i = 0; i < document.getElementById('menu-container').children.length; i++) {
+                document.getElementById('menu-container').children[i].classList.remove('selected', 'initial');
+            }
+        }
+
         setChildren(this.botContainer, []);
         setChildren(this.midContainer, []);
         setChildren(this.topContainer, []);
@@ -64,13 +95,14 @@ export default class LandingPage {
 
     renderAlternateElements = () => {
         setChildren(this.topContainer, this.logo);
+        setChildren(this.midContainer, this.menuContainer);
         setChildren(this.overlayLight, [this.topContainer, this.midContainer, this.botContainer]);
         setChildren(this.el, this.overlayLight);
     }
 
     renderInitialElements = () => {
         setChildren(this.botContainer, this.startButton);
-        setChildren(this.midContainer, this.titleSection);
+        setChildren(this.midContainer, [this.titleSection, this.menuContainer]);
         setChildren(this.topContainer, this.logo);
         setChildren(this.overlay, [this.topContainer, this.midContainer, this.botContainer]);
         setChildren(this.el, this.overlay);
--- src/components/room_scene.js
+++ src/components/room_scene.js
@@ -1,6 +1,7 @@
 var THREE = require('three');
 var OBJLoader = require('three-obj-loader');
 var MTLLoader = require('three-mtl-loader');
+import {TextureLoader} from 'three-full';
 OBJLoader(THREE);
 
 import {TweenLite} from 'gsap';
@@ -17,8 +18,10 @@ export default class RoomScene {
         this.landingPage = new LandingPage();
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer();
-        this.el = this.renderer.domElement
+        
+        this.el = this.renderer.domElement;
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 2000);
+
         this.objectLoader = new THREE.OBJLoader(manager);
         this.mtlLoader = new MTLLoader(manager);
         this.activeMenu = constants.INITIAL;
@@ -26,7 +29,10 @@ export default class RoomScene {
 
     onmount() {
         this.createScene();
+        this.camera.position.set(-1, 0, -1);
         this.renderRoom();
+
+        this.renderContents();
         // Start animation loop    
         this.animate()
     }
@@ -35,9 +41,9 @@ export default class RoomScene {
         mount(document.body, this.loadingScreen);
         this.scene.background = new THREE.Color( 0xffffff );
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-
+        
         // Simulate sunlight
-        var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1 );
+        var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
         hemiLight.position.set(0, 400, 0);
         this.scene.add(hemiLight);
     }
@@ -62,6 +68,7 @@ export default class RoomScene {
                 object.rotation.y -= Math.PI / 2;
 
                 this.scene.add(object);
+                // this.renderer.render(this.scene, this.camera);
                 this.renderer.render(this.scene, this.camera);
                 mount(document.body, this.landingPage);
                 unmount(document.body, this.loadingScreen);
@@ -78,9 +85,44 @@ export default class RoomScene {
         })
     }
 
+    renderContents = () => {
+        //// ABOUT
+        var aboutGeometry = new THREE.PlaneBufferGeometry(1.5, 1.5);
+        const aboutTexture = new TextureLoader().load('/images/about_text.png');
+
+        // Create and set shader
+        let aboutShader = constants.aboutShader;
+        aboutShader.uniforms.texture.value = aboutTexture;
+        var aboutMaterial = new THREE.ShaderMaterial(aboutShader);
+
+        // Make plane transparent
+        aboutMaterial.transparent = true;
+        aboutMaterial.opacity = 0.5;
+        aboutMaterial.blending = THREE.SubtractiveBlending;//THREE.AdditiveBlending;
+        
+        var aboutMesh = new THREE.Mesh(aboutGeometry, aboutMaterial);
+        aboutMesh.position.set(2.5, -0.2, -3.3);
+        aboutMesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -1.5708);
+
+        this.scene.add(aboutMesh);
+
+        //// APPS
+        // Mind Archive
+        let mindArchiveGeometry = new THREE.PlaneBufferGeometry(0.6, 0.8);
+        let mindArchiveMaterial = new THREE.MeshBasicMaterial();
+        let mindArchiveMesh = new THREE.Mesh(mindArchiveGeometry, mindArchiveMaterial);
+
+        mindArchiveMesh.position.set(-3.05, 0, -5.6);
+        mindArchiveMesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), 1.5708);
+        mindArchiveMesh.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), -0.1);
+        this.scene.add(mindArchiveMesh);
+
+        this.renderer.render(this.scene, this.camera)
+    }
+
     animate = () => {
         requestAnimationFrame(this.animate);
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.render(this.scene, this.camera)
     }
 
     resizeRenderer = () => {
@@ -88,16 +130,16 @@ export default class RoomScene {
     }
 
     rotateCamera = (x, y) => {
-        var sensitivity = 0.00002;
-        
+        var sensitivity = 0.00003;
         if (this.activeMenu === constants.INITIAL) {
             this.camera.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), -y * sensitivity)
             this.camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
+
             this.renderer.render(this.scene, this.camera)
         } else if (this.activeMenu === constants.ABOUT) {
             this.camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
             this.camera.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), -y * sensitivity)
-            this.renderer.render(scene, this.camera)
+            this.renderer.render(this.scene, this.camera)
         } else if (this.activeMenu === constants.APPS || this.activeMenu === constants.BLOG) {
             this.camera.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -x * sensitivity)
             this.camera.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), y * sensitivity)
@@ -131,7 +173,6 @@ export default class RoomScene {
     }
 
     moveCamera = (menu) => {
-        
         // Trigger page change
         this.landingPage.selectActiveMenu(menu);
         mount(document.body, this.landingPage);
