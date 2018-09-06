8d58d36a1c8bdb016c70529ced67e1167340f539
Andre Vasconcelos <andre.tito@architectgroup.com>
2018-09-06 11:14:37 +0900

    Removing semicolons cause they useless

--- src/components/loading_screen.js
+++ src/components/loading_screen.js
@@ -1,30 +1,30 @@
-import WhiteLogo from '../assets/images/plain_white.png';
-import {el, setChildren} from 'redom';
+import WhiteLogo from '../assets/images/plain_white.png'
+import {el, setChildren} from 'redom'
 
 export default class LoadingScreen {
     constructor() {
-        this.progress = 0;
-        this.el = el('#loading-page-container');
-        this.percentage = el('span#percentage', `${this.progress}%`);
-        this.loadingComponent = el('.loading-component-container');
-        this.image = el('img', {src: WhiteLogo});
-        this.progressBar = el('#loading-component-progress');
+        this.progress = 0
+        this.el = el('#loading-page-container')
+        this.percentage = el('span#percentage', `${this.progress}%`)
+        this.loadingComponent = el('.loading-component-container')
+        this.image = el('img', {src: WhiteLogo})
+        this.progressBar = el('#loading-component-progress')
 
-        this.updateProgress = this.updateProgress.bind(this);
+        this.updateProgress = this.updateProgress.bind(this)
     }
 
     onmount() {
-        setChildren(this.loadingComponent, [this.image, this.progressBar]);
-        setChildren(this.el, [this.loadingComponent, this.percentage]);
+        setChildren(this.loadingComponent, [this.image, this.progressBar])
+        setChildren(this.el, [this.loadingComponent, this.percentage])
     }
 
     updateProgress = (newProgress) => {
         // Update percentage text
-        let percentage = document.getElementById('percentage');
+        let percentage = document.getElementById('percentage')
         percentage.innerHTML = `${newProgress}%`
 
         // resize progress height
         let progress = document.getElementById('loading-component-progress')
-        progress.style.height = `${120 * (newProgress/100)}px`;
+        progress.style.height = `${120 * (newProgress/100)}px`
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import {el, mount} from 'redom';
-import RoomScene from './components/room_scene';
-import * as constants from './utils/constants';
-import './css/loading_screen.css';
-import './css/style.css';
+import {el, mount} from 'redom'
+import RoomScene from './components/room_scene'
+import * as constants from './utils/constants'
+import './css/loading_screen.css'
+import './css/style.css'
 
 class Root {
     constructor() {
-        this.roomSceneObj = new RoomScene();
-        this.el = el('#main-div');
+        this.roomSceneObj = new RoomScene()
+        this.el = el('#main-div')
     }
  
     onmount() {
-        mount(this.el, this.roomSceneObj);
+        mount(this.el, this.roomSceneObj)
         this.registerEventListeners()
     }
 
     registerEventListeners = () => {
         window.addEventListener('resize', () => { 
-            this.roomSceneObj.resizeRenderer();
-        });
+            this.roomSceneObj.resizeRenderer()
+        })
 
         document.addEventListener('mousemove', (event) => {
-            var x = event.movementX;
-            var y = event.movementY;
-            this.roomSceneObj.rotateCamera(x, y);
+            var x = event.movementX
+            var y = event.movementY
+            this.roomSceneObj.rotateCamera(x, y)
         })
 
         window.addEventListener('wheel', () => {
@@ -31,15 +31,14 @@ class Root {
         })
         
         document.addEventListener('click', (event) => {
-            // Clicked menu/button to go to About section
             if(event.target.id.includes(constants.ABOUT)) {
-                this.roomSceneObj.moveCamera(constants.ABOUT);
+                this.roomSceneObj.moveCamera(constants.ABOUT)
             } else if (event.target.id.includes(constants.APPS)) {
-                this.roomSceneObj.moveCamera(constants.APPS);
+                this.roomSceneObj.moveCamera(constants.APPS)
             } else if (event.target.id.includes(constants.BLOG)) {
-                this.roomSceneObj.moveCamera(constants.BLOG);
+                this.roomSceneObj.moveCamera(constants.BLOG)
             } else if (event.target.id.includes(constants.CONTACTS)) {
-                this.roomSceneObj.moveCamera(constants.CONTACTS);
+                this.roomSceneObj.moveCamera(constants.CONTACTS)
             } else if (event.target.id.includes(constants.INITIAL)) {
                 this.roomSceneObj.moveCamera(constants.INITIAL)
             }
@@ -47,5 +46,5 @@ class Root {
     }
 }
 
-const root = new Root();
-mount(document.body, root);
+const root = new Root()
+mount(document.body, root)
